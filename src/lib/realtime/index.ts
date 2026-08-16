56391e03f8695091a17b46c50b474f71801615b2
alifiroozi <47670437+alifiroozii@users.noreply.github.com>
2026-08-16 22:47:52 +0330

    chore: normalize realtime exports

--- src/lib/realtime/index.ts
+++ src/lib/realtime/index.ts
@@ -2,7 +2,4 @@ export type { RealtimeEventName, RealtimeEventPayloads } from "./events";
 export { invalidateQueriesForRealtimeEvent } from "./query-invalidation";
 export { formatRoomName, authorizeRoomJoin } from "./rooms";
 export { realtimeServer, publishRealtimeEvent } from "./socket-server";
-export type {
-  PublishedRealtimeEvent,
-  SocketConnectionSession,
-} from "./socket-server";
+export type { PublishedRealtimeEvent, SocketConnectionSession } from "./socket-server";
